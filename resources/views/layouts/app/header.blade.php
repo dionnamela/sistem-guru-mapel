ccccfefe725e11dd660bf391f8cf5762cce26950
gideonnamlea4-prog <gideon.namlea4@guru.smp.belajar.id>
2026-05-28 14:05:13 +0800

    feat: menu data siswa di side bar

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -55,8 +55,9 @@
                 <flux:sidebar.group :heading="__('Platform')">
                     <flux:sidebar.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard')  }}
-                    </flux:sidebar.item>
-                </flux:sidebar.group>
+                    </flux:sidebar.item>                    <flux:sidebar.item icon="users" :href="route('student')" :current="request()->routeIs('student')" wire:navigate>
+                        {{ __('Data Siswa') }}
+                    </flux:sidebar.item>                </flux:sidebar.group>
             </flux:sidebar.nav>
 
             <flux:spacer />
--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -15,6 +15,9 @@
                     <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
+                    <flux:sidebar.item icon="users" :href="route('student')" :current="request()->routeIs('student')" wire:navigate>
+                        {{ __('Data Siswa') }}
+                    </flux:sidebar.item>
                 </flux:sidebar.group>
             </flux:sidebar.nav>
 
